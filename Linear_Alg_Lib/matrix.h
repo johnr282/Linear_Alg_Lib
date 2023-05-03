dd6478f7e6ca66fef29b5918bd17dd8f1544db7d
johnr282 <johnrose020802@gmail.com>
2023-05-02 17:32:40 -0700

    Started implementing matrix inheritance

--- Linear_Alg_Lib/matrix.h
+++ Linear_Alg_Lib/matrix.h
@@ -8,74 +8,30 @@
 #include "structs.h"
 
 
-// Header file for linear algebra library; contains definitions of all public classes
+// Contains base Matrix class and the derived classes DenseMatrix, SparseMatrix, DiagonalMatrix
 
 
-// Dense matrix class
+
 template <typename T>
 class Matrix {
 
-// ********** Member variables **********
 private:
 
-
-	// Vector to store data 
-	std::vector<T> data;
-
-	// Number of rows and columns
+	// Number of rows/columns and total number of elements
 	size_t rows;
 	size_t cols;
-
-	// Number of elements
 	size_t size;
 
-	// Determines method of data storage, either column major or row major
-	// Matrices are column major by default
-	StorageType storage_type;
-
-
-// ********** Member functions **********
 public:
-	
-	// Constructor; data_in is assumed to be in correct order given by storage_type_in
-	Matrix(const std::vector<T> &data_in, const size_t rows_in, const size_t cols_in, 
-		const StorageType storage_type_in = StorageType::ColumnMajor)
-		: data{ data_in }, rows{ rows_in }, cols{ cols_in }, storage_type{ storage_type_in } {
-		size = rows_in * cols_in;
 
-		// Check that size of data vector matches rows_in * cols_in
-		if (data_in.size() != Matrix::size) {
-			std::cerr << "Invalid Matrix initialization input vector; "
-				<< "size of input vector must equal number of rows times number of columns\n";
-			exit(1);
-		}
-	}
+	// Constructor
+	Matrix(size_t rows_in, size_t cols_in)
+		: rows(rows_in), cols(cols_in), size(rows_in* cols_in) { }
 
 
-	// Constructor (no data vector provided); default initializes all elements
-	Matrix(const size_t rows_in, const size_t cols_in, const StorageType storage_type_in = StorageType::ColumnMajor) 
-		: rows{ rows_in }, cols{ cols_in }, storage_type{ storage_type_in } {
-		size = rows_in * cols_in;
-		std::vector<T> data_in(size);
-		data = data_in;
-	}
+	virtual void setData() = 0;
 
-
-	// Getter and setter functions
-	
-	std::vector<T> getData() {
-		return data;
-	}
-
-	void setData(const std::vector<T> &data_in) {
-		// Check that size of data_in matches matrix size
-		if (data_in.size() != Matrix::size) {
-			std::cerr << "Invalid new Matrix data vector; "
-				<< "size of new Matrix data vector must equal current size of Matrix\n";
-			exit(1);
-		}
-		data = data_in;
-	}
+	// Getter functions common to all derived matrix classes
 
 	size_t getRows() {
 		return rows;
@@ -89,20 +45,150 @@ public:
 		return size;
 	}
 
-	StorageType getStorageType() {
-		return storage_type;
+};
+
+
+// DenseMatrix class, derived from base Matrix class; stores all data, zero and nonzero,
+// in one-dimensional array; supports both RowMajor and ColumnMajor storage, but defaults
+// to ColumnMajor
+template <typename T>
+class DenseMatrix : public Matrix<T> {
+
+private:
+
+	// Vector to store data 
+	std::vector<T> data;
+
+	// Determines method of data storage, either column major or row major
+	// Matrices are column major by default
+	StorageType storage_type;
+
+public:
+
+	// Constructor
+	DenseMatrix(std::vector<T> data_in, size_t rows_in, size_t cols_in, StorageType storage_type_in = StorageType::ColumnMajor)
+		: Matrix(rows_in, cols_in), data(data_in), storage_type(storage_type_in) { 
+		// Check that size of data vector matches rows_in * cols_in
+			if (data_in.size() != rows_in * cols_in) {
+				std::cerr << "Invalid Matrix initialization input vector; "
+					<< "size of input vector must equal number of rows times number of columns\n";
+				exit(1);
+			}
 	}
 
+	// Constructor with no given data vector; default initializes all elements in matrix
+	DenseMatrix(size_t rows_in, size_t cols_in, StorageType storage_type_in = StorageType::ColumnMajor)
+		: Matrix(rows_in, cols_in), storage_type(storage_type_in) {
+		std::vector<T> data_in(rows_in * cols_in);
+		data = data_in;
+	}
 
-
-
-
+	/*void setData() {
+		std::cout << "Hello\n";
+	}*/
 };
 
 
 
 
-
+//// Base Matrix class; abstract class
+//template <typename T>
+//class Matrix {
+//
+//// ********** Member variables **********
+//private:
+//
+//	// Number of rows and columns
+//	size_t rows;
+//	size_t cols;
+//
+//	// Number of elements
+//	size_t size;
+//
+//
+//// ********** Member functions **********
+//public:
+//
+//	// Constructor
+//	Matrix(size_t rows_in, size_t cols_in)
+//		: rows{ rows_in }, cols{ cols_in }, size{ rows_in * cols_in } { }
+//
+//	// virtual void setData() = 0;
+//
+//	size_t getRows() {
+//		return rows;
+//	}
+//
+//	size_t getCols() {
+//		return cols;
+//	}
+//
+//	size_t getSize() {
+//		return size;
+//	}
+//
+//};
+//
+//
+// DenseMatrix class, derived from base Matrix class; stores all data, zero and nonzero,
+// in one-dimensional array; supports both RowMajor and ColumnMajor storage, but defaults
+// to ColumnMajor
+//template <typename T>
+//class DenseMatrix : public Matrix<T> {
+//
+//// ********** Member variables **********
+//private:
+//
+//	// Vector to store data 
+//	std::vector<T> data;
+//
+//	// Determines method of data storage, either column major or row major
+//	// Matrices are column major by default
+//	StorageType storage_type;
+//
+//
+//// ********** Member functions **********
+//public:
+//
+//	// Constructor; data_in is assumed to be in correct order given by storage_type_in
+//	DenseMatrix(const std::vector<T>& data_in, const size_t rows_in, const size_t cols_in,
+//		const StorageType storage_type_in = StorageType::ColumnMajor)
+//		: Matrix{ rows_in, cols_in }, data{ data_in }, storage_type{ storage_type_in } {
+//
+//		// Check that size of data vector matches rows_in * cols_in
+//		if (data_in.size() != DenseMatrix::size) {
+//			std::cerr << "Invalid Matrix initialization input vector; "
+//				<< "size of input vector must equal number of rows times number of columns\n";
+//			exit(1);
+//		}
+//	}
+//
+//	//// Constructor (no data vector provided); default initializes all elements
+//	//DenseMatrix(const size_t rows_in, const size_t cols_in,
+//	//	const StorageType storage_type_in = StorageType::ColumnMajor)
+//	//	: Matrix{ rows_in, cols_in }, storage_type{ storage_type_in } {
+//
+//	//}
+//
+//	std::vector<T> getData() {
+//		return data;
+//	}
+//
+//	void setData(const std::vector<T>& data_in) {
+//		// Check that size of data_in matches matrix size
+//		if (data_in.size() != DenseMatrix::size) {
+//			std::cerr << "Invalid new Matrix data vector; "
+//				<< "size of new Matrix data vector must equal current size of Matrix\n";
+//			exit(1);
+//		}
+//		data = data_in;
+//	}
+//
+//	StorageType getStorageType() {
+//		return storage_type;
+//	}
+//
+//};
 
 
 
--- Linear_Alg_Lib/tests.h
+++ Linear_Alg_Lib/tests.h
@@ -17,33 +17,34 @@
 
 // Dense Matrix constructor tests
 void testDenseMatrixCtor() {
-	// Test first constructor with data vector
-	std::vector<int> data(12, 1);
-	Matrix<int> mat(data, 3, 4, StorageType::RowMajor);
-	// Check if data is correct
-	assert(mat.getData() == data);
-	assert(mat.getRows() == 3);
-	assert(mat.getCols() == 4);
-	assert(mat.getSize() == 12);
-	assert(mat.getStorageType() == StorageType::RowMajor);
+	DenseMatrix<int> mat(3, 3);
+	//// Test first constructor with data vector
+	//std::vector<int> data(12, 1);
+	//DenseMatrix<int> mat(data, 3, 4, StorageType::RowMajor);
+	//// Check if data is correct
+	//assert(mat.getData() == data);
+	//assert(mat.getRows() == 3);
+	//assert(mat.getCols() == 4);
+	//assert(mat.getSize() == 12);
+	//assert(mat.getStorageType() == StorageType::RowMajor);
 
-	// Test with default storage type
-	std::vector<double> data2(12, 1.5);
-	Matrix<double> mat2(data2, 3, 4);
-	assert(mat2.getStorageType() == StorageType::ColumnMajor);
+	//// Test with default storage type
+	//std::vector<double> data2(12, 1.5);
+	//DenseMatrix<double> mat2(data2, 3, 4);
+	//assert(mat2.getStorageType() == StorageType::ColumnMajor);
 
-	// Test second constructor with no data vector
-	Matrix<uint32_t> mat3(4, 5);
+	//// Test second constructor with no data vector
+	/*DenseMatrix<uint32_t> mat3(4, 5);
 	assert(mat3.getRows() == 4);
 	assert(mat3.getCols() == 5);
 	assert(mat3.getSize() == 20);
-	assert(mat3.getStorageType() == StorageType::ColumnMajor);
+	assert(mat3.getStorageType() == StorageType::ColumnMajor);*/
 
-	Matrix<float> mat4(6, 6, StorageType::RowMajor);
+	/*DenseMatrix<float> mat4(6, 6, StorageType::RowMajor);
 	assert(mat4.getRows() == 6);
 	assert(mat4.getCols() == 6);
 	assert(mat4.getSize() == 36);
-	assert(mat4.getStorageType() == StorageType::RowMajor);
+	assert(mat4.getStorageType() == StorageType::RowMajor);*/
 }
 
 
