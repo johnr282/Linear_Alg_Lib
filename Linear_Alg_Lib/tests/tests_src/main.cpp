c4e1740305b5860632a6e8e06cb77c19cfe24543
johnrose <johnrose@umich.edu>
2023-10-01 20:34:26 -0400

    Added tests/benchmarks command line arguments

--- Linear_Alg_Lib/tests/tests_src/main.cpp
+++ Linear_Alg_Lib/tests/tests_src/main.cpp
@@ -5,18 +5,77 @@
 #include "../tests_include/benchmarks.h"
 
 // ------------------------------------------------------------------
-// Runs all tests
+// Runs tests and benchmarks
 // ------------------------------------------------------------------
 
-int	main(int argc, char* argv[]) {
-	testMathVector();
-	testDenseMatrix();
+bool processArgs(const int argc,
+	char* argv[],
+	bool& run_tests,
+	bool& run_benchmarks);
 
-	std::cout << "All tests complete\n";
+int	main(int argc, char* argv[]) 
+{
+	bool run_tests = false;
+	bool run_benchmarks = false;
+	bool valid_args = processArgs(argc, argv, run_tests, run_benchmarks);
 
-	runBenchmarks();
+	if (!valid_args)
+		return 1;
 
-	std::cout << "All benchmarks complete\n";
-	
+	if (run_tests)
+	{
+		testMathVector();
+		testDenseMatrix();
+
+		std::cout << "All tests complete\n";
+	}
+
+	if (run_benchmarks)
+	{
+		runBenchmarks();
+
+		std::cout << "All benchmarks complete\n";
+	}
+
 	return 0;
+}
+
+// Process command line arguments and sets parameters run_tests and
+// run benchmarks; returns true if args are valid, false if not
+bool processArgs(const int argc, 
+	char* argv[], 
+	bool& run_tests, 
+	bool& run_benchmarks)
+{
+	std::string usage =
+		"Usage: -t to run tests, -b to run benchmarks, -tb to run both tests and benchmarks\n";
+
+	if (argc != 2)
+	{
+		std::cout << usage;
+		return false;
+	}
+
+	std::string options(argv[1]);
+
+	if (options == "-tb")
+	{
+		run_tests = true;
+		run_benchmarks = true;
+	}
+	else if (options == "-t")
+	{
+		run_tests = true;
+	}
+	else if (options == "-b")
+	{
+		run_benchmarks = true;
+	}
+	else
+	{
+		std::cout << usage;
+		return false;
+	}
+
+	return true;
 }
